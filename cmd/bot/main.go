c437881362cfe0d9971c5627ad77a83975b434ea
sashaem <mm-56@bk.ru>
2024-10-25 14:41:38 +0300

    test bot example

---
+++ cmd/bot/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+// Send any text message to the bot after the bot has been started
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New("7738140505:AAHIKcbMwlhQjcZmUz1tcmFdn9q6CQoWfVw", opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sashaem1/Amnesia_bot
+
+go 1.23.2
+
+require github.com/go-telegram/bot v1.9.1 // indirect
